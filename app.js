26256ec378c93a0fc44190c4c7719f73fe044c01
AaronKwong929 <AaronKwong929@gmail.com>
2019-06-05 23:49:37 +0800

    midlewares

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('./src/db/mongoose');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
-const controller = require('./src/controller');
-const templating = require('./src/templating');
+const controller = require('./src/middlewares/controller');
+const templating = require('./src/middlewares/templating');
 const isProduction = process.env.NODE_ENV === 'production';
 
 if (!isProduction) {
-    const staticFiles = require('./src/static-files');
-    app.use(staticFiles('/static/', __dirname + '/static'));
+    const staticFiles = require('./src/middlewares/static-files');
+    app.use(staticFiles('/src/statics/', __dirname + '/src/statics'));
 }
 
 app.use(bodyParser());
@@ -20,5 +20,7 @@ app.use(
     })
 );
 app.use(controller());
-app.listen(3000);
-console.log('app started at port 3000');
+// app.listen(3000);
+app.listen(3000, () => {
+    console.log(`app started at port 3000`);
+});
